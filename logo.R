4ec13adc2349fbdc8d75618d3537951b7c13ff92
Mark Hanly <m.hanly@unsw.edu.au>
2021-02-17 09:27:21 +1100

    Wider plot, small point style, narrower border, consistent border, title colour

--- logo.R
+++ logo.R
@@ -19,23 +19,23 @@ show_col(viridis_pal()(20))
 df <- tibble(
   x = 1:100,
   d = x > 50,
-  yhat = 0.2 * x + 5 * d + 0.2 * x * d,
+  yhat = 0.2 * x + 5 * d + 0.3 * x * d,
   y = yhat + rnorm(length(x), 0, 4))
 
 # Plot  
 rdd <- ggplot(df, aes(x = x, color = x)) +
-  geom_point(aes(y=y), size = .01) +
+  geom_point(aes(y=y), size = .01, shape = 20) +
   geom_line(aes(y=yhat, group = d), size = 0.8) +
-  geom_vline(xintercept = 50, color = '#B8DE29FF', size = .1) +
+  geom_vline(xintercept = 50, color = '#1F968BFF', size = .1) +
   scale_color_continuous(type = "viridis") +
   theme_void() +
   theme(legend.position = 'none')
 
 
 # Create sticker
-sticker(rdd, package="RUNAPP", p_size = 5, p_color = '#FDE725FF',
-        s_x=1, s_y=0.8, s_width=1, s_height=1,
+sticker(rdd, package="RUNAPP", p_size = 5, p_color = '#1F968BFF',
+        s_x=1, s_y=0.8, s_width=1.4, s_height=1,
         h_fill = 'black',
-        h_size = 1.5,
-        h_color = 'grey40',
+        h_size = .6,
+        h_color = '#1F968BFF',
         filename="runapp-logo.png") 
